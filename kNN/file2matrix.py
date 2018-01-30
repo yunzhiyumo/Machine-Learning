c14efb0c5ba35def58ddf21288f7fc8a7569e98c
Richard Liu <pm123456@aliyun.com>
2018-01-30 23:35:50 +0800

    Update file2matrix.py

--- kNN/file2matrix.py
+++ kNN/file2matrix.py
@@ -2,12 +2,12 @@
 
 
 from numpy import *         
-import operator
+import operator #本模块主要包括一些Python内部操作符对应的函数。这些函数主要分为几类：对象比较、逻辑比较、算术运算和序列操作。
 
 # 数据预处理
 def file2matrix(filename):
-    fr = open(filename)
-    arrayOLines = fr.readlines()
+    fr = open(filename) #open()是python的函数
+    arrayOLines = fr.readlines() #读取所有行，按行读入
     numberOfLines = len(arrayOLines) #返回行数
     returnMat = zeros((numberOfLines,3)) #建立符合格式的空矩阵
     classLabelVector = [] #建立符合格式的空列表
